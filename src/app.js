2360791fc40ae8c114015c2676dca599d3f8590d
ouhouh1936 <ouhouh1936@gmail.com>
2021-08-31 13:09:45 +0900

    dfdf

--- src/app.js
+++ src/app.js
@@ -6,16 +6,24 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const globalRouter = require("./router/globalRouter");
 const connect = require("../db");
-connect();
+const expressSession = require("express-session");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.set("view engine", "pug");
 app.use(morgan(`dev`));
 app.use(express.static(path.join(__dirname, "/assets")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+  expressSession({
+    secret: "@#@$MYSIGN#@$#$",
+    resave: false,
+    saveUninitiqlized: true,
+  })
+);
+connect();
 
 app.use("/", globalRouter);
 
